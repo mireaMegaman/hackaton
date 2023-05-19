7184f7e196b6083e9cf614e85edb37e752d723ea
Lunal <100156578+AlexeyLunyakov@users.noreply.github.com>
2023-05-19 18:26:32 +0300

    Delete augmentations directory

--- augmentations/crawler.py
+++
@@ -1,5 +0,0 @@
-from datetime import date
-from icrawler.builtin import FlickrImageCrawler
-
-flickr_crawler = FlickrImageCrawler('17d082f450af18012ae480394dbd0883', storage={'root_dir': 'D:\Education\Hacaton\Small'})
-flickr_crawler.crawl(max_num=2700, text="""Bewick's swan""", max_size=(4096, 3072))
